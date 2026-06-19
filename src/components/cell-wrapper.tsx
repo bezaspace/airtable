ede541a667d196c961cc1f634ca29b0a8fc24614
Harsha <harsha8643@gmail.com>
2026-06-19 23:56:05 +0530

    Update cell wrapper

--- src/components/cell-wrapper.tsx
+++ src/components/cell-wrapper.tsx
@@ -222,7 +222,8 @@ export function CellWrapper({
             className={cn(
               "fixed top-1/2 left-1/2 z-50 -translate-x-1/2 -translate-y-1/2",
               "max-w-[80vw] max-h-[80vh] overflow-auto",
-              "bg-popover text-popover-foreground text-sm",
+              "bg-black text-white text-sm",
+              "border border-white/20",
               "outline-none duration-100",
               "data-open:animate-in data-open:fade-in-0 data-open:zoom-in-95",
               "data-closed:animate-out data-closed:fade-out-0 data-closed:zoom-out-95"
